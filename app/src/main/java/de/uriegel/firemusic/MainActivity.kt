887624e91ca24c9fa32c8b319519019fbe274ed2
Uwe Riegel <uriegel@hotmail.de>
2020-12-30 07:37:10 +0100

    URL settings

--- app/src/main/java/de/uriegel/firemusic/MainActivity.kt
+++ app/src/main/java/de/uriegel/firemusic/MainActivity.kt
@@ -1,11 +1,33 @@
 package de.uriegel.firemusic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ActivityEx(), CoroutineScope {
+
+    override val coroutineContext = Dispatchers.Main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        launch {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(this@MainActivity)
+            var url = preferences.getString("url", "")
+            if (url!!.length < 6) {
+                activityRequest(Intent(this@MainActivity, SettingsActivity::class.java))
+                url = preferences.getString("url", "")
+            }
+            MainActivity.url = url!!
+        }
+    }
+
+    companion object {
+        lateinit var url: String
     }
 }
